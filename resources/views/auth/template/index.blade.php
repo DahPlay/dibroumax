6335a74ac090322670cd76886272774cecce6b93
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2025-03-31 07:06:32 -0400

    feat: added bootstrap js

--- resources/views/auth/template/index.blade.php
+++ resources/views/auth/template/index.blade.php
@@ -7,6 +7,8 @@
     @yield('content')
 
     @include('auth.template.javascript')
+    <!-- Bootstrap CSS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
