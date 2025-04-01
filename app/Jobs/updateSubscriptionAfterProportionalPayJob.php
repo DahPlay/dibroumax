e8791845ef9d59c3e39406730c41a8f4263fa876
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2025-04-01 17:51:54 -0400

    feat: add job to update subscription after proportional payment

---
+++ app/Jobs/updateSubscriptionAfterProportionalPayJob.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use App\Services\PaymentGateway\Connectors\AsaasConnector;
+use App\Services\PaymentGateway\Gateway;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class updateSubscriptionAfterProportionalPayJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(private readonly Order $order)
+    {
+    }
+
+    public function handle(): void
+    {
+        $adapter = app(AsaasConnector::class);
+        $gateway = new Gateway($adapter);
+
+        $nextDueDate = $this->getNewDate($this->order->cycle);
+        $data = [
+            'id' => $this->order->subscription_asaas_id,
+            'value' => $this->order->original_plan_value,
+            'nextDueDate' => now()->addDays($nextDueDate),
+            'description' => "Assinatura com 0 dias grátis",
+            'externalReference' => 'Pedido: ' . $this->order->id,
+        ];
+
+        $response = $gateway->subscription()->update($this->order->subscription_asaas_id, $data);
+
+        if ($response['object'] === 'subscription') {
+            $this->order->update([
+                'value' => $this->order->original_plan_value,
+                'changed_plan' => false
+            ]);
+        }
+    }
+
+    private function getNewDate($cycle): int
+    {
+        return match ($cycle) {
+            'WEEKLY' => 7,
+            'BIWEEKLY' => 14,
+            'MONTHLY' => 30,
+            'BIMONTHLY' => 60,
+            'QUARTERLY' => 120,
+            'SEMIANNUALLY' => 182,
+            'YEARLY' => 365,
+        };
+    }
+}
