b52e778847a1b884dd526e30eddaea466ed3a9df
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2025-04-02 09:21:04 -0400

    feat: command to tests

---
+++ app/Console/Commands/playCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\BackOrderOldPlanJob;
+use App\Models\Order;
+use Illuminate\Console\Command;
+
+class playCommand extends Command
+{
+    protected $signature = 'play';
+
+    protected $description = 'Command to tests';
+
+    public function handle (): void
+    {
+        $order = Order::find(10);
+        BackOrderOldPlanJob::dispatch($order);
+    }
+}
