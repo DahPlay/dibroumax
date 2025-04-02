e59a10142111f671b30104d6563dbb948b55544c
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2025-04-02 15:20:26 -0400

    feat: add PackageSeeder to populate package data

---
+++ database/seeders/PackageSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Package;
+use Illuminate\Database\Seeder;
+
+class PackageSeeder extends Seeder
+{
+    public function run (): void
+    {
+        $packages = [
+            ['name' => 'Dahplay Superior', 'cod' => 59, 'is_active' => true],
+            ['name' => 'Dahplay Completo', 'cod' => 76, 'is_active' => true],
+            ['name' => 'Dahplay Premium', 'cod' => 130, 'is_active' => true],
+            ['name' => 'Dahplay Premiere', 'cod' => 118, 'is_active' => true],
+            ['name' => 'Dahplay Telecine', 'cod' => 129, 'is_active' => true],
+        ];
+
+        foreach ($packages as $package) {
+            Package::updateOrCreate(
+                ['name' => $package['name']],
+                $package
+            );
+        }
+    }
+}
