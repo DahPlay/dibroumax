c3da4e9a55da9e348c81a2e784edb9b9a425f0d9
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2025-04-02 11:47:48 -0400

    feat: update

--- app/Http/Controllers/Panel/PackagesController.php
+++ app/Http/Controllers/Panel/PackagesController.php
@@ -92,81 +92,22 @@ class PackagesController extends Controller
 
     public function edit($id): View
     {
-        $plan = $this->model->find($id);
+        $package = $this->model->find($id);
 
-        return view('panel.plans.local.index.modals.edit', compact("Package"));
+        return view('panel.packages.local.index.modals.edit', compact("package"));
     }
 
 
-    public function update($id): JsonResponse
+    public function update($id, PackageRequest $request): RedirectResponse
     {
-        dd('update');;
-        $plan = $this->model->find($id);
+       $model = $this->model->find($id)->update($request->validated());
 
-        if ($plan) {
-            $data = $this->request->only([
-                'name',
-                'value',
-                'description',
-                'cycle',
-                'is_active',
-                'is_best_seller',
-                'billing_type',
-                'free_for_days',
-            ]);
-
-            $validator = Validator::make($data, [
-                'name' => ['required', 'string', 'max:255'],
-                'value' => ['required', 'string', 'min:0'],
-                'description' => ['nullable', 'string'],
-                'name' => ['required', 'string'],
-                'is_active' => ['nullable', 'string'],
-                'is_best_seller' => ['nullable', 'string'],
-                'billing_type' => ['string'],
-                'free_for_days' => ['integer'],
-            ]);
-
-            if (count($validator->errors()) > 0) {
-                return response()->json([
-                    'status' => 400,
-                    'errors' => $validator->errors(),
-                ]);
-            }
-
-            $data['is_active'] = isset($data['is_active']) ? 1 : 0;
-            $data['is_best_seller'] = isset($data['is_best_seller']) ? 1 : 0;
-
-            $plan->update($data);
-
-            $plan->benefits()->delete();
-
-            if ($this->request->filled('benefits') && count($this->request->input('benefits')) > 0 && !is_null($this->request->input('benefits')[0])) {
-                foreach ($this->request->benefits as $benefit) {
-                    $plan->benefits()->create(['description' => $benefit]);
-                }
-            }
-
-            if ($plan) {
-                return response()->json([
-                    'status' => '200',
-                    'message' => 'Ação executada com sucesso!'
-                ]);
-            } else {
-                return response()->json([
-                    'status' => '400',
-                    'errors' => [
-                        'message' => ['Erro executar a ação, tente novamente!']
-                    ]
-                ]);
-            }
-        } else {
-            return response()->json([
-                'status' => '400',
-                'errors' => [
-                    'message' => ['Os dados não foram encontrados!']
-                ]
-            ]);
+        if ($model) {
+            toastr('Salvo!');
+            return redirect()->route('panel.packages.index');
         }
+        toastr('Tente novamente ou entre em contato com o administrador do sistema!');
+        return redirect()->route('panel.packages.index');
     }
 
 
---
+++ resources/views/panel/packages/local/index/modals/edit.blade.php
@@ -0,0 +1,19 @@
+<div class="modal-content">
+    <div class="modal-header">
+        <h4 class="modal-title">Editar</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <form action="{{ route('panel.packages.update', $package->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        @include("includes.forms.$routeCrud")
+
+        <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+            <button type="submit" class="btn btn-primary btn-submit">Editar</button>
+        </div>
+    </form>
+</div>
