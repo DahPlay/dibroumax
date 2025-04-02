a6f6fa926bbd43a0d335ee927205a9aba8f42441
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2025-04-02 09:56:07 -0400

    feat: implement PackagesController for package management

---
+++ app/Http/Controllers/Panel/PackagesController.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace App\Http\Controllers\Panel;
+
+use App\Http\Controllers\Controller;
+use App\Models\Package;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Yajra\DataTables\Facades\DataTables;
+
+class PackagesController extends Controller
+{
+    public function __construct(public Package $packages, public Request $request)
+    {
+        $this->middleware('can:admin');
+    }
+
+    public function index(): View
+    {
+        return view($this->request->route()->getName());
+    }
+
+    public function loadDatatable(): JsonResponse
+    {
+        $packages = $this->packages
+            ->get();
+
+        return DataTables::of($packages)
+            ->addColumn('checkbox', function ($package) {
+                return view('panel.packages.local.index.datatable.checkbox', compact('package'));
+            })
+            ->editColumn('id', function ($package) {
+                return view('panel.packages.local.index.datatable.id', compact('package'));
+            })
+            ->editColumn('created_at', function ($package) {
+                return $package->created_at ? date('d/m/Y H:i:s', strtotime($package->created_at)) : 'Sem data';
+            })
+            ->filterColumn('created_at', function ($query, $value) {
+                $query->whereRaw("DATE_FORMAT(created_at,'%d/%m/%Y %H:%i:%s') like ?", ["%$value%"]);
+            })
+            ->editColumn('is_active', function ($item) {
+                return view('panel.packages.local.index.datatable.is_active', compact('item'));
+            })
+            ->addColumn('action', function ($package) {
+                $loggedId = auth()->user()->id;
+
+                return view('panel.packages.local.index.datatable.action', compact('package', 'loggedId'));
+            })
+            ->toJson();
+    }
+
+   public function create(): View
+    {
+        $package = $this->packages;
+
+        return view('panel.packages.local.index.modals.create', compact('package'));
+    }
+
+    /* public function store(): JsonResponse
+    {
+        $data = $this->request->only([
+            'name',
+            'value',
+            'description',
+            'cycle',
+            'is_active',
+            'is_best_seller',
+            'billing_type',
+            'free_for_days',
+        ]);
+
+        $validator = Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'value' => ['required', 'string', 'min:0'],
+            'description' => ['nullable', 'string'],
+            'name' => ['required', 'string'],
+            'is_active' => ['nullable', 'string'],
+            'is_best_seller' => ['nullable', 'string'],
+            'billing_type' => ['string'],
+            'free_for_days' => ['integer'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $data['is_active'] = isset($data['is_active']) ? 1 : 0;
+        $data['is_best_seller'] = isset($data['is_best_seller']) ? 1 : 0;
+
+        $plan = $this->model->create($data);
+
+        if ($this->request->filled('benefits')) {
+            foreach ($this->request->benefits as $benefit) {
+                $plan->benefits()->create(['description' => $benefit]);
+            }
+        }
+
+        if ($plan) {
+            return response()->json([
+                'status' => '200',
+                'message' => 'Ação executada com sucesso!'
+            ]);
+        } else {
+            return response()->json([
+                'status' => '400',
+                'errors' => [
+                    'message' => ['Erro executar a ação, tente novamente!']
+                ]
+            ]);
+        }
+    }
+
+    public function edit($id): View
+    {
+        $plan = $this->model->find($id);
+
+        return view('panel.plans.local.index.modals.edit', compact("Package"));
+    }
+
+    public function duplicate(): View
+    {
+        $plan = $this->model->find($this->request->id);
+
+        return view('panel.plans.local.index.modals.duplicate', compact('Package'));
+    }
+
+    public function update($id): JsonResponse
+    {
+        $plan = $this->model->find($id);
+
+        if ($plan) {
+            $data = $this->request->only([
+                'name',
+                'value',
+                'description',
+                'cycle',
+                'is_active',
+                'is_best_seller',
+                'billing_type',
+                'free_for_days',
+            ]);
+
+            $validator = Validator::make($data, [
+                'name' => ['required', 'string', 'max:255'],
+                'value' => ['required', 'string', 'min:0'],
+                'description' => ['nullable', 'string'],
+                'name' => ['required', 'string'],
+                'is_active' => ['nullable', 'string'],
+                'is_best_seller' => ['nullable', 'string'],
+                'billing_type' => ['string'],
+                'free_for_days' => ['integer'],
+            ]);
+
+            if (count($validator->errors()) > 0) {
+                return response()->json([
+                    'status' => 400,
+                    'errors' => $validator->errors(),
+                ]);
+            }
+
+            $data['is_active'] = isset($data['is_active']) ? 1 : 0;
+            $data['is_best_seller'] = isset($data['is_best_seller']) ? 1 : 0;
+
+            $plan->update($data);
+
+            $plan->benefits()->delete();
+
+            if ($this->request->filled('benefits') && count($this->request->input('benefits')) > 0 && !is_null($this->request->input('benefits')[0])) {
+                foreach ($this->request->benefits as $benefit) {
+                    $plan->benefits()->create(['description' => $benefit]);
+                }
+            }
+
+            if ($plan) {
+                return response()->json([
+                    'status' => '200',
+                    'message' => 'Ação executada com sucesso!'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => '400',
+                    'errors' => [
+                        'message' => ['Erro executar a ação, tente novamente!']
+                    ]
+                ]);
+            }
+        } else {
+            return response()->json([
+                'status' => '400',
+                'errors' => [
+                    'message' => ['Os dados não foram encontrados!']
+                ]
+            ]);
+        }
+    }
+
+    public function delete($id): View
+    {
+        $plan = $this->model->find($this->request->id);
+
+        return view('panel.plans.local.index.modals.delete', compact("Package"));
+    }
+
+    public function destroy(): JsonResponse
+    {
+        $plan = $this->model->find($this->request->id);
+
+        if ($plan) {
+            $delete = $plan->delete();
+
+            if ($delete) {
+                return response()->json([
+                    'status' => '200',
+                    'message' => 'Ação executada com sucesso!'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => '400',
+                    'errors' => [
+                        'message' => ['Erro executar a ação, tente novamente!']
+                    ],
+                ]);
+            }
+        } else {
+            return response()->json([
+                'status' => '400',
+                'errors' => [
+                    'message' => ['Os dados não foram encontrados!']
+                ],
+            ]);
+        }
+    }
+
+    public function deleteAll(): View
+    {
+        $itens = $this->request->checkeds;
+
+        session()->put('ids', $itens);
+
+        return view('panel.plans.local.index.modals.remove-all', compact("itens"));
+    }
+
+    public function destroyAll(): JsonResponse
+    {
+        foreach (session()->get('ids') as $item) {
+            $item = $this->model->find($item["id"]);
+
+            if ($item) {
+                $item->delete();
+
+                if (!$item) {
+                    return response()->json([
+                        'status' => '400',
+                        'errors' => [
+                            'message' => ['Erro executar a ação, tente novamente!']
+                        ],
+                    ]);
+                }
+            } else {
+                return response()->json([
+                    'status' => '400',
+                    'errors' => [
+                        'message' => ['Os dados não foram encontrados!']
+                    ],
+                ]);
+            }
+        }
+
+        return response()->json([
+            'status' => '200',
+            'message' => 'Ação executada com sucesso!'
+        ]);
+    }*/
+}
