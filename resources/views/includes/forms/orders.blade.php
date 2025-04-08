84392cb172cda1d7ef66ef3cc2da90410fce2cc5
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2025-04-08 09:21:21 -0400

    refactor: restrict visibility of customer and subscription IDs to admin and developer roles

--- resources/views/includes/forms/orders.blade.php
+++ resources/views/includes/forms/orders.blade.php
@@ -1,28 +1,29 @@
 <div class="modal-body">
-    <div class="row d-flex align-items-center">
-        <div class="form-group col-6">
-            <label for="customer_asaas_id" class="col-form-label">Customer Asaas ID:</label>
-            <div class="input-group">
-                <input type="text" id="customer_asaas_id" class="form-control" name="customer_asaas_id"
-                    placeholder="Customer Asaas ID" value="{{ $order->customer_asaas_id }}" disabled>
+    @canany(['admin', 'developer'])
+        <div class="row d-flex align-items-center">
+            <div class="form-group col-6">
+                <label for="customer_asaas_id" class="col-form-label">Customer Asaas ID:</label>
+                <div class="input-group">
+                    <input type="text" id="customer_asaas_id" class="form-control" name="customer_asaas_id"
+                           placeholder="Customer Asaas ID" value="{{ $order->customer_asaas_id }}" disabled>
+                </div>
+            </div>
+
+            <div class="form-group col-6">
+                <label for="subscription_asaas_id" class="col-form-label">Subscription Asaas ID:</label>
+                <div class="input-group">
+                    <input type="text" id="subscription_asaas_id" class="form-control" name="subscription_asaas_id"
+                           placeholder="Subscription Asaas ID" value="{{ $order->subscription_asaas_id }}" disabled>
+                </div>
             </div>
         </div>
-
-        <div class="form-group col-6">
-            <label for="subscription_asaas_id" class="col-form-label">Subscription Asaas ID:</label>
-            <div class="input-group">
-                <input type="text" id="subscription_asaas_id" class="form-control" name="subscription_asaas_id"
-                    placeholder="Subscription Asaas ID" value="{{ $order->subscription_asaas_id }}" disabled>
-            </div>
-        </div>
-    </div>
-
+    @endcanany
     <div class="row">
         <div class="form-group col-6">
             <label for="value" class="col-form-label">Preço:</label>
             <div class="input-group">
                 <input type="text" id="value" class="form-control" name="value" placeholder="Preço"
-                    value="{{ number_format($order->value, 2, ',', '.') }}" disabled>
+                       value="{{ number_format($order->value, 2, ',', '.') }}" disabled>
             </div>
         </div>
 
@@ -30,7 +31,7 @@
             <label for="cycle" class="col-form-label">Ciclo de pagamento:</label>
             <div class="input-group">
                 <input type="text" id="cycle" class="form-control" name="cycle" placeholder="Ciclo"
-                    value="{{ $order->cycle }}" disabled>
+                       value="{{ $order->cycle }}" disabled>
             </div>
         </div>
     </div>
@@ -40,7 +41,7 @@
             <label for="billing_type" class="col-form-label">Tipo de pagamento:</label>
             <div class="input-group">
                 <input type="text" id="billing_type" class="form-control" name="billing_type"
-                    placeholder="Billing Type" value="{{ $order->billing_type }}" disabled>
+                       placeholder="Billing Type" value="{{ $order->billing_type }}" disabled>
             </div>
         </div>
 
@@ -48,7 +49,7 @@
             <label for="next_due_date" class="col-form-label">Grátis até:</label>
             <div class="input-group">
                 <input type="date" id="next_due_date" class="form-control" name="next_due_date"
-                    placeholder="Next Due Date" value="{{ $order->next_due_date }}" disabled>
+                       placeholder="Next Due Date" value="{{ $order->next_due_date }}" disabled>
             </div>
         </div>
 
@@ -56,7 +57,7 @@
             <label for="status" class="col-form-label">Status:</label>
             <div class="input-group">
                 <input type="text" id="status" class="form-control" name="status" placeholder="Status"
-                    value="{{ $order->status }}" disabled>
+                       value="{{ $order->status }}" disabled>
             </div>
         </div>
     </div>
@@ -65,7 +66,8 @@
         <label for="description" class="col-form-label">Descrição:</label>
         <div class="form-group col-12">
             <div class="input-group">
-                <textarea id="description" class="form-control" name="description" rows="3" placeholder="Description" disabled>{{ $order->description }}</textarea>
+                <textarea id="description" class="form-control" name="description" rows="3" placeholder="Description"
+                          disabled>{{ $order->description }}</textarea>
             </div>
         </div>
     </div>
--- resources/views/panel/orders/local/index/datatable/action.blade.php
+++ resources/views/panel/orders/local/index/datatable/action.blade.php
@@ -3,8 +3,19 @@
         <i class="fa fa fa-ellipsis-v text-dark"></i>
     </a>
     <div class="dropdown-menu dropdown-menu-lg">
+        @can('user')
+            <a href='javascript:;' class='btn-change btn btn-danger dropdown-item' data-id='{{ $order->id }}'
+               data-url='/{{ $routeCrud }}/changePlan'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="bi bi-arrow-left-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                          d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
+                </svg>
+                <span class="ml-2">Trocar de plano</span>
+            </a>
+        @endcan
         <a href='javascript:;' class='btn-show btn btn-info dropdown-item' data-id='{{ $order->id }}'
-            data-url='/{{ $routeCrud }}/show'>
+           data-url='/{{ $routeCrud }}/show'>
             <i class='fa fa-eye'></i>
             <span class="ml-2">Ver</span>
         </a>
@@ -18,17 +29,9 @@
         @endcan --}}
 
         <a href='javascript:;' class='btn-cancel btn btn-danger dropdown-item' data-id='{{ $order->id }}'
-            data-url='/{{ $routeCrud }}/cancel'>
+           data-url='/{{ $routeCrud }}/cancel'>
             <i class='fa fa-lock'></i>
             <span class="ml-2">Cancelar</span>
-        </a>
-
-        <a href='javascript:;' class='btn-change btn btn-danger dropdown-item' data-id='{{ $order->id }}'
-           data-url='/{{ $routeCrud }}/changePlan'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
-            </svg>
-            <span class="ml-2">Trocar de plano</span>
         </a>
 
 
