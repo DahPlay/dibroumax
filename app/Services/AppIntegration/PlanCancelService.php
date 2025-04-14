aa293c097208e2f067da2a54c03ef458ce4cf694
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2025-04-14 10:17:06 -0400

    rf: refactor plan cancellation logic and add suspension package handling

--- app/Services/AppIntegration/PlanCancelService.php
+++ app/Services/AppIntegration/PlanCancelService.php
@@ -2,15 +2,11 @@
 
 namespace App\Services\AppIntegration;
 
-use App\Models\Package;
 use App\Services\YouCast\Plan\PlanCancel;
-use App\Services\YouCast\Plan\PlanCreate;
 use Illuminate\Support\Facades\Log;
 
 class PlanCancelService
 {
-    //todo: se eu chamo este service de cancelamento para cancelar o plano desativado, mas no final eu ativo ele novamente.
-    //todo: preciso corrigir isso em todos os lugares
     public function __construct(
         protected readonly array $packageCodes,
         protected readonly int|string $viewerId,
@@ -32,9 +28,5 @@ class PlanCancelService
                 "PlanCancelService - Pacote Código: {$code} cancelado na API externa para a Customer {$this->viewerId}."
             );
         }
-
-        //adiciono o plano que não vê nada só para o cliente não ficar com a plataforma vazia
-        $dahPlayDesativado = Package::where('cod', '136')->first();
-        (new PlanCreate())->handle($this->viewerId, $dahPlayDesativado->cod);
     }
 }
