c0d974fd5fb25a63f83951a90d95c83a35933f5b
Chomotech <125891107+alexandreChromotech@users.noreply.github.com>
2025-07-29 18:08:30 -0300

    Adicionando link boleto 9

--- resources/views/panel/orders/local/index/datatable/payment_asaas_id.blade.php
+++ resources/views/panel/orders/local/index/datatable/payment_asaas_id.blade.php
@@ -1,14 +1,18 @@
 @php
-    $idSemPrefixo = str_replace('pay_', '', $item->payment_asaas_id);
-    $environment = app()->isLocal() ? 'sandbox' : 'production';
-    $urlBase = config("asaas.{$environment}.fatura_url");
+    $id = $item->payment_asaas_id;
 @endphp
 
-@if ($item->payment_asaas_id)
-    <a href="{{ $urlBase }}/i/{{ $idSemPrefixo }}" target="_blank"
-        class="btn btn-sm btn-primary" data-toggle="tooltip" title="Ver fatura">
-        <i class="fa fa-file-invoice-dollar"></i> Ver fatura
-    </a>
+@if (!$id)
+    <span class="badge bg-secondary">SEM FATURA</span>
 @else
-    <span class="badge badge-secondary">SEM FATURA</span>
+    @php
+        $idSemPrefixo = str_replace('pay_', '', $id);
+        $environment = app()->isLocal() ? 'sandbox' : 'production';
+        $urlBase = config("asaas.{$environment}.fatura_url");
+        $url = $urlBase . '/i/' . $idSemPrefixo;
+    @endphp
+
+    <a href="{{ $url }}" target="_blank" class="btn btn-sm btn-primary">
+        <i class="fas fa-file-invoice-dollar me-1"></i> Ver Fatura
+    </a>
 @endif
